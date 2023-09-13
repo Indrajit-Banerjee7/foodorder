2ac8976daad61d2a0eb4274af90d90540843fafc
Indrajit <indrajit30623@gmail.com>
2023-09-13 12:33:10 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className='h-screen w-screen flex items-center justify-center text-5xl font-mono bg-red-300'>
-        <p>A happy starting of our project</p>
+        <p>A happy starting of our project! lets go!</p>
         </div>
     </>
   )
