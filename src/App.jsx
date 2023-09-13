a3e0008984786d2b3084a51a18609fad3e2e95a1
Indrajit <indrajit30623@gmail.com>
2023-09-13 12:24:31 +0530

    initilasized the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='h-screen w-screen flex items-center justify-center text-5xl font-mono bg-red-300'>
+        <p>A happy starting of our project</p>
+        </div>
+    </>
+  )
+}
+
+export default App
