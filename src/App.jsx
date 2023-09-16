178aea32c61a084edad83d700558c7bb287298cd
Indrajit <indrajit30623@gmail.com>
2023-09-16 18:47:22 +0530

    created navbar componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
 
   return (
     <>
+    <Navbar/>
       <div className='h-screen w-screen flex items-center justify-center text-5xl font-mono bg-red-300'>
-        <p>A happy starting of our project! lets go!</p>
+        <p className=' text-black'></p>
         </div>
     </>
   )
