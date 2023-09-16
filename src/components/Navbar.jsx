cb561b856c69b270e78a1478b7c9b7ab19e09ff4
Indrajit <indrajit30623@gmail.com>
2023-09-16 18:52:26 +0530

    made navbar sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar sticky top-0 bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
